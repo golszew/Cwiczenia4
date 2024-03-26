ae50e89fab537471921caa35564eda63d1197de5
s27691 <s27691@pjwstk.edu.pl>
2024-03-26 11:08:06 +0100

    Testy

---
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -0,0 +1,50 @@
+namespace LegacyApp.Tests;
+
+public class UserServiceTests
+{
+    [Fact]
+    public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
+    {
+        //Arrange
+
+        var userService = new UserService();
+
+        //Act
+
+        var result = userService.AddUser(
+            null,
+            "Kowalski",
+            "kow@kow.com",
+            DateTime.Parse("2000-01-01"),
+            123);
+
+        //Assert
+        
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ThrowsExceptionWhenClientDoesNotExist()
+    {
+        //Arrange
+
+        var userService = new UserService();
+
+        //Act
+
+        
+
+        var action = () => {userService.AddUser(
+            "Jan",
+            "Kowalski",
+            "kow@kow.com",
+            DateTime.Parse("2000-01-01"),
+            100);};
+        
+        //Assert
+        
+        Assert.Throws<ArgumentException>(action);
+    }
+    
+    //dot Cover potem tests coverAll
+}
