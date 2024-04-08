cb9dcb24a77791c7a0b117d2a3f3688bfbfb99da
golszew <s27691@pjwstk.edu.pl>
2024-04-08 17:33:24 +0200

    Commit przed parkiem

--- zadanie/LegacyApp.Tests/UserServiceTests.cs
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -24,6 +24,48 @@ public class UserServiceTests
     }
     
     [Fact]
+    public void AddUser_ReturnsFalseWhenSecondNameIsEmpty()
+    {
+        //Arrange
+
+        var userService = new UserService();
+
+        //Act
+
+        var result = userService.AddUser(
+            "Jan",
+            null,
+            "kow@kow.com",
+            DateTime.Parse("2000-01-01"),
+            123);
+
+        //Assert
+        
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenEmailIsInWrongFormat()
+    {
+        //Arrange
+
+        var userService = new UserService();
+
+        //Act
+
+        var result = userService.AddUser(
+            "Jan",
+            "Kowalski",
+            "kow-kow-com",
+            DateTime.Parse("2000-01-01"),
+            123);
+
+        //Assert
+        
+        Assert.False(result);
+    }
+    
+    [Fact]
     public void AddUser_ThrowsExceptionWhenClientDoesNotExist()
     {
         //Arrange
@@ -46,8 +88,27 @@ public class UserServiceTests
         Assert.Throws<ArgumentException>(action);
     }
     
+    [Fact]
+    public void AddUser_ReturnsFalseWhenUserIsYoungerThanTwentyTwo()
+    {
+        //Arrange
+        var userService = new UserService();
+
+        //Act
+        DateTime ago = DateTime.Now.AddYears(-21).AddDays(1);
+        var result = userService.AddUser("Jan", "Kowalski", "kow@kow.com", ago, 123);
+
+        //Assert
+        Assert.False(result);
+    }
+    
     //dot Cover potem tests coverAll
     
     //najpierw nadpisac wszystkie testy
     //potem zrobic refactor addusera
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
 }
